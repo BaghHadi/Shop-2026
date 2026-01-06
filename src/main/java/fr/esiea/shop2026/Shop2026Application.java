a491fdf61ff1366b2614160a8e9dfdc46f726de5
BAGHDADLI Abdelhadi <abaghdadli@et.esiea.fr>
2026-01-06 16:54:14 +0100

    Initial commit

---
+++ src/main/java/fr/esiea/shop2026/Shop2026Application.java
@@ -0,0 +1,13 @@
+package fr.esiea.shop2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shop2026Application.class, args);
+    }
+
+}
